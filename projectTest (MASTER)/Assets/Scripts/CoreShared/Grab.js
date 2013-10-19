c1618102808a3a5b1e9909744a82fc32327d29a6
adkaplan <adkaplan@virginia.edu>
2013-10-19 15:42:06 -0400

    Prep for Code Jam

--- projectTest (MASTER)/Assets/Scripts/CoreShared/Grab.js
+++ projectTest (MASTER)/Assets/Scripts/CoreShared/Grab.js
@@ -12,68 +12,64 @@ var v:Vector3;
 var offset:Vector3;
 var lastPos:Vector3;
 
+var grabPow:int;
+var throwPow:int;
+
 function Start () {
 	selfShader = gameObject.GetComponentInChildren(typeof(Renderer));
 	changeColor(baseColor);
 	lastPos = transform.position;
 	//JSLeap = GameObject.Find("JSLeapController").GetComponent(typeof(JSLeap));
 }
-function Update () {
-	if(grabbed) {
-		if(v.magnitude<=.001) {
+function Update() {
+	if (grabbed) {
+		if (v.magnitude <= .001) {
 			lastPos = transform.position;
-			v = Vector3.identity;
+			v = new Vector3(0, 0, 0);
 		}
 		changeColor(highlightColor);
 
-		Then This
-		Vector3Pasda
-		sdas
-		diffasd
-		asd
-		asd
-		asd
-		asda
-		sd.
-		
 		//transform.position = anchorPoint; //This would be absolute positioning
-		
-		var diff:Vector3 = (transform.position - anchorPoint);
-		transform.position -= (diff * Mathf.Pow(diff.magnitude,1.3) * .05);
+		var grabFriction = .94;
+		var diff: Vector3 = (transform.position - anchorPoint);
+		var grabPow = 1.3;
+		transform.position -= (diff * Mathf.Pow(diff.magnitude, grabPow) * 0.05);
 		transform.position += v;
-		v = (transform.position - lastPos) * .94;
+		v = (transform.position - lastPos) * grabFriction;
 		lastPos = transform.position;
 		return;
 	} else {
-		transform.position += v;
-		v = (transform.position - lastPos) * .91;
+		var throwPow = 1.3;
+		transform.position -= (diff * Mathf.Pow(diff.magnitude, throwPow) * 0.05);
+		var throwFriction = .91;
+		v = (transform.position - lastPos) * throwFriction;
 		lastPos = transform.position;
-		}
-	
-	if(hoverCount == 0) return;
-	if(hoverCount== .5) {
+	}
+	if (hoverCount == 0) return;
+	if (hoverCount == .5) {
 		hoverCount = 0;
 	} else {
 		hoverCount -= .5;
-		if(hoverCount >= 9.5) {
+		if (hoverCount >= 9.5) {
 			changeColor(highlightColor);
 		} else {
-			changeColor(Color.Lerp(baseColor,highlightColor,hoverCount/10.0));
+
+			changeColor(Color.Lerp(baseColor, highlightColor, hoverCount / 10.0));
 		}
 	}
-	
+
 }
 
 function hover() {
-	hoverCount ++;
-	if(hoverCount>10) hoverCount = 10;
+	hoverCount++;
+	if (hoverCount > 10) hoverCount = 10;
 }
 
-function changeColor(c:Color) {
-	selfShader.renderer.material.SetColor("_Color",c);
+function changeColor(c: Color) {
+	selfShader.renderer.material.SetColor("_Color", c);
 }
 
-function handUpdate(pos:Vector3) {
+function handUpdate(pos: Vector3) {
 
 	anchorPoint = pos;
 }
