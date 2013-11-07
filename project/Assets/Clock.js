fe60f7dc1262baad6e261ebac460f8977acc1a5d
dylanhellems <dylanhellems@gmail.com>
2013-11-07 18:23:03 -0500

    Added Timer

---
+++ project/Assets/Clock.js
@@ -0,0 +1,34 @@
+﻿private var myTimer : float;
+private var displaySeconds : int;
+private var displayMinutes : int;
+
+function Start () {
+	myTimer = 0;
+}
+
+//A simple timer
+function Update () {
+  myTimer += Time.deltaTime;
+  onGUI();
+ 
+}
+
+function onGUI () {
+//display the timer
+	if (!Trigger.isTriggered) {
+	    myTimer = Mathf.CeilToInt(myTimer);
+	    displaySeconds = myTimer % 60;
+	    displayMinutes = myTimer / 60; 
+	    
+	    text = String.Format ("{0:00}:{1:00}", displayMinutes, displaySeconds); 
+	    guiText.text = text;
+ 	} else {
+    	text = "You Win! /n" + myTimer;
+    }
+
+}
+
+function onTriggerEnter () {
+	text = "You Win! /n" + myTimer;
+}
+
---
+++ project/Assets/Trigger.js
@@ -0,0 +1,14 @@
+﻿static var isTriggered = false;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function onTriggerEnter (thing : Collider) {
+	isTriggered = true;
+	Debug.Log("triggered");
+}
